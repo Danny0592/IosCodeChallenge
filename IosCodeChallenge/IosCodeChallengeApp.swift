0cfcbec45c24bab473dcd99e8c53f59dfaf952d4
daniel ortiz millan <danielortizmillan@MacBook-Air-de-daniel.local>
2024-04-12 17:22:06 -0600

    first commit

---
+++ IosCodeChallenge/IosCodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosCodeChallengeApp.swift
+//  IosCodeChallenge
+//
+//  Created by daniel ortiz millan on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct IosCodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
